075b0a01eea3e906f51f5b44b1eb8c7b64da5409
alu20902426a <alu20902426a@192.168.201.110>
2018-03-14 19:42:42 +0100

    Add Constructor

--- MyFirstGitProject/src/GitProject.java
+++ MyFirstGitProject/src/GitProject.java
@@ -2,5 +2,10 @@
 public class GitProject {
 	private String param1;
 	private String param2;
+	public GitProject(String param1, String param2) {
+		super();
+		this.param1 = param1;
+		this.param2 = param2;
+	}
 	
 }
